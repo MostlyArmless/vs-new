008964a6d1687322c99bed1a252d9fc106088825
Mike Thiem <Mike.Thiem@ihsmarkit.com>
2019-02-01 11:17:33 -0700

    Submitted to npm

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # vs-new
-Set up a blueprint folder ready to go for TypeScript development.
+Set up a blueprint folder ready to go for TypeScript Node.js development in VS Code.
 
 This package lets you deploy a preconfigured directory structure including:
-* .git folder
+* .git folder, with .gitignore that ignores /node_modules/
 * .vscode folder with preconfigured launch.json and tasks.json
 * preconfigured tsconfig.json
 * blank README.md
 * package.json preconfigured with mocha and chai pre-installed
 * test folder with a boilerplate mocha test.ts file
+
+## Installation
+<code>npm i -g vs-new</code>
+
+## Usage
+From your parent Projects directory: <code>vsnew my-new-project</code> will create a new directory named my-new-project and will insert that name into the package.json file. All other boilerplate files will be copied into that directory.
--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
+#!/usr/bin/env node
 var ncp = require('ncp').ncp;
+var jsonfile = require('jsonfile');
+var os = require('os');
+var fs = require('fs');
+var path = require('path');
+
+let packageName = process.argv[2];
+try {
+    fs.mkdirSync(packageName)    
+} catch (err) {
+    console.error(`There's already a folder with the package name "${packageName}" in this directory. Aborting.`);
+    process.exit(-1);
+}
+
+let packageDir = path.join(process.cwd(), packageName);
 
 ncp.limit = 16;
-
-ncp(source, destination, function(err) {
+ncp(path.join(__dirname,'../blueprint/'), packageDir, function(err) {
     if (err) {
         return console.error(err);
     }
+
+    // Replace the package name and author name
+    let packageFilePath = path.join(packageDir, '/package.json');
+    console.log(packageFilePath);
+    let packageJsonContents = jsonfile.readFileSync(packageFilePath);
+
+    
+    console.log(`Package name = "${packageName}", author = "${os.userInfo().username}"`);
+    packageJsonContents.name = packageName;
+    packageJsonContents.author = os.userInfo().username;
+
+    jsonfile.writeFileSync(packageFilePath, packageJsonContents, { spaces: 2 });
+
     console.log('Done.');
 });
